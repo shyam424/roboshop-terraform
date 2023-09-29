2fb0ef3d59fde535ec6f20c29af008ed8f62b86a
Shyam <shyamneo24@gmail.com>
2023-09-29 12:16:16 +0530

    terraform-module-tfvars

--- main.tf
+++ main.tf
@@ -1,11 +1,6 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
-
 module "components" {
 
- # source    = "git::https://github.com/shyam424/tf-module-basic-test.git"
+  source    = "git::https://github.com/shyam424/tf-module-basic-test.git"
   for_each  = var.components
 
   zone_id           = var.zone_id
